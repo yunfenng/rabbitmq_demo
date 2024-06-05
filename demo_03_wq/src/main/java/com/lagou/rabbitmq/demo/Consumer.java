c2a65e5e1df97a7b370b7c991f7dfd31503913e9
fenng <xqjia123@gmail.com>
2024-06-05 10:56:34 +0800

    RabbitMQ 工作队列模式

---
+++ demo_03_wq/src/main/java/com/lagou/rabbitmq/demo/Consumer.java
@@ -0,0 +1,41 @@
+package com.lagou.rabbitmq.demo;
+
+import com.rabbitmq.client.CancelCallback;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.Delivery;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @Author: Jaa
+ * @Description:
+ * @Date 2024/6/5
+ */
+public class Consumer {
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUri("amqp://root:123456@47.103.155.255:5672/%2f");
+
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+
+        channel.queueDeclare("queue.wq", true, false, false, null);
+
+        channel.basicConsume("queue.wq", new DeliverCallback() {
+            @Override
+            public void handle(String consumerTag, Delivery message) throws IOException {
+                System.out.println("推送过来的消息：" + new String(message.getBody(), StandardCharsets.UTF_8));
+            }
+        }, new CancelCallback() {
+            @Override
+            public void handle(String consumerTag) throws IOException {
+                System.out.println("Cancel 消费者取消消费：" + consumerTag);
+            }
+        });
+    }
+}
---
+++ demo_03_wq/src/main/java/com/lagou/rabbitmq/demo/Producer.java
@@ -0,0 +1,40 @@
+package com.lagou.rabbitmq.demo;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * @Author: Jaa
+ * @Description:
+ * @Date 2024/6/5
+ */
+public class Producer {
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUri("amqp://root:123456@47.103.155.255:5672/%2f");
+
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+
+        // 声明一个消息队列
+        channel.queueDeclare("queue.wq", true, false, false, null);
+        // 声明 direct 交换器
+        channel.exchangeDeclare("ex.wq", BuiltinExchangeType.DIRECT, true, false, null);
+        // 将消息队列绑定到指定的交换器，并指定路由键
+        channel.queueBind("queue.wq", "ex.wq", "key.wq");
+        for (int i = 0; i < 15; i++) {
+            channel.basicPublish(
+                    "ex.wq",
+                    "key.wq",
+                    null,
+                    ("工作队列：" + i).getBytes("utf-8"));
+        }
+
+        channel.close();
+        connection.close();
+
+    }
+}
